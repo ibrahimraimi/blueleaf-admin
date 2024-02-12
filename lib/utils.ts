bbc878a6dd8aede7cbadae0937127814c78b1af4
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-02-12 12:47:03 +0100

    add zod auth schema and clerk errors validations

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,40 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { isClerkAPIResponseError } from "@clerk/nextjs";
+import { type ClassValue, clsx } from "clsx";
+import { toast } from "sonner";
+import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export function catchClerkError(err: unknown) {
+  const unknownErr = "Something went wrong, please try again later.";
+
+  if (err instanceof z.ZodError) {
+    const errors = err.issues.map((issue) => {
+      return issue.message;
+    });
+    return toast(errors.join("\n"));
+  } else if (isClerkAPIResponseError(err)) {
+    return toast.error(err.errors[0]?.longMessage ?? unknownErr);
+  } else {
+    return toast.error(unknownErr);
+  }
+}
+
+export const authSchema = z.object({
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+  password: z
+    .string()
+    .min(8, {
+      message: "Password must be at least 8 characters long",
+    })
+    .max(100)
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/, {
+      message:
+        "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character",
+    }),
+});
