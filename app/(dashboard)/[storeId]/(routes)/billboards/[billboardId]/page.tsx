2df75b509a204daec6ab97c79c6a57cd50448f59
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-02-13 13:23:18 +0100

    fix default export errors

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,34 +1,25 @@
-import { format } from "date-fns";
-
 import prismadb from "@/lib/prisma";
-import { BillboardClient } from "@/components/dashboard/billboards/client";
-import { BillboardColumn } from "@/components/dashboard/billboards/columns";
 
-export default async function BillboardsPage({
+import { BillboardForm } from "@/components/dashboard/billboards/billboard-form";
+
+const BillboardPage = async ({
   params,
 }: {
-  params: { storeId: string };
-}) {
-  const billboard = await prismadb.billboard.findMany({
+  params: { billboardId: string };
+}) => {
+  const billboard = await prismadb.billboard.findUnique({
     where: {
-      storeId: params.storeId,
-    },
-    orderBy: {
-      createdAt: "desc",
+      id: params.billboardId,
     },
   });
-
-  const formattedBillboards: BillboardColumn[] = billboard.map((item) => ({
-    id: item.id,
-    label: item.label,
-    createdAt: format(item.createdAt, "MMMM do, yyyy"),
-  }));
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formattedBillboards} />
+        <BillboardForm initialData={billboard} />
       </div>
     </div>
   );
-}
+};
+
+export default BillboardPage;
